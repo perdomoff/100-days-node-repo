d9c4af77719d4e605f80a16fa54f4bb0be0aa43c
freddyperdomo_omni <freddyp@omnifederal.com>
2023-05-09 23:18:14 -0400

    First commit

---
+++ day1-testing-folder-creation-commit/index.js
@@ -0,0 +1,10 @@
+// const inputYear = "1991";
+// console.log(inputYear);
+
+// console.log("it runs", inputYear);
+// console.log(typeof NaN);
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
